49cb3bec4dbe44d0fbe29ca959728cb6fe47cbab
Leandro <qazuor@gmail.com>
2025-03-22 17:49:57 +0000

    fix issue with translations

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,30 +1,20 @@
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
-import { mainTranslations, zodTranslations } from './locales';
+import { QRFTTranslations } from './locales';
 
-const zodResources = Object.entries(zodTranslations).reduce(
+const QRFTResources = Object.entries(QRFTTranslations).reduce(
     (acc: { [key: string]: { translation: typeof value } }, [lang, value]) => {
         acc[lang] = { translation: value };
         return acc;
     },
     {}
 );
-
-const mainResources = Object.entries(mainTranslations).reduce(
-    (acc: { [key: string]: { translation: typeof value } }, [lang, value]) => {
-        acc[lang] = { translation: value };
-        return acc;
-    },
-    {}
-);
-
-const resources = Object.assign({}, mainResources, zodResources);
 
 i18n.use(LanguageDetector)
     .use(initReactI18next)
     .init({
-        resources: resources,
+        resources: QRFTResources,
         fallbackLng: 'en',
         interpolation: {
             escapeValue: false
--- src/i18n/locales/index.ts
+++ src/i18n/locales/index.ts
@@ -2,18 +2,6 @@ import en from './en.json';
 import es from './es.json';
 
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-export const zodTranslations: Record<SupportedLangs, Record<string, any>> = {
-    en: en.zod,
-    es: es.zod
-};
-
-// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-export const mainTranslations: Record<SupportedLangs, Record<string, any>> = {
-    en: en.main,
-    es: es.main
-};
-
-// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 export const QRFTTranslations: Record<SupportedLangs, Record<string, any>> = {
     en,
     es
