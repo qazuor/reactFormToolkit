72ea285c9616e285a30c17be15e04eeb08859440
Leandro <qazuor@gmail.com>
2025-03-23 20:18:55 +0000

    include animations css in the build

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,11 +5,20 @@ export default defineConfig((options) => ({
     entry: ['src/index.ts'],
     format: ['cjs', 'esm'],
     outDir: 'dist',
+    outExtension: ({ format }) => ({
+        js: format === 'cjs' ? '.cjs' : '.js'
+    }),
     dts: true,
     splitting: true,
     sourcemap: !options.watch,
     clean: true,
     treeshake: true,
     external: ['react', 'react-dom'],
-    alias: { '@': resolve(__dirname, './src') }
+    alias: { '@': resolve(__dirname, './src') },
+    async onSuccess() {
+        // Copy animations.css to dist
+        await import('fs/promises').then(fs =>
+            fs.copyFile('src/styles/animations.css', 'dist/animations.css')
+        );
+    }
 }));
