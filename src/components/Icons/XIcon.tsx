f9ab69e289566049c429b0d839447d123a64092b
Leandro <qazuor@gmail.com>
2025-03-26 06:50:54 +0000

    use camelCase

--- src/components/Icons/XIcon.tsx
+++ src/components/Icons/XIcon.tsx
@@ -18,9 +18,9 @@ export function XIcon({ title, className }: IconProps): JSX.Element {
             viewBox='0 0 24 24'
             fill='none'
             stroke='currentColor'
-            stroke-width='2'
-            stroke-linecap='round'
-            stroke-linejoin='round'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
             className={className}
         >
             <title>{title || defaultTitle}</title>
